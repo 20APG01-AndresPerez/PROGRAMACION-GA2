a9f1776811270071849715f4f61bc7d3eb19c1a1
20APG01-AndresPerez <a.perez.5adb@gmail.com>
2026-04-11 08:04:04 -0600

    Add files via upload

---
+++ Menu_eje4.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Menu de Opciones");
+        Console.WriteLine("1- Hamburguesa");
+        Console.WriteLine("2- Pizza");
+        Console.WriteLine("3- Pollo Frito");
+        Console.WriteLine("4- Ensalada");
+
+        int opc;
+
+        Console.WriteLine("Digite un numero del 1 al 4");
+        opc = Convert.ToInt32(Console.ReadLine());
+
+        switch (opc)
+        {
+            case 1:
+                Console.WriteLine("Hamburguesa");
+                break;
+            case 2:
+                Console.WriteLine("Pizza");
+                break;
+            case 3:
+                Console.WriteLine("Pollo Frito");
+                break;
+            case 4:
+                Console.WriteLine("Ensalada");
+                break;
+            default:
+                Console.WriteLine("Opcion invalida");
+                break;
+        }
+    }
+}
---
+++ Menu_eje4.psc
@@ -0,0 +1,23 @@
+Algoritmo Menu_eje4
+	Definir opc como Entero
+	Escribir "Menu de opciones"
+	Escribir "1- Hamburguesa"
+	Escribir "2- Pizza"
+	Escribir "3- Pollo Frito"
+	Escribir "4- Ensalada"
+	Escribir Sin Saltar "Digite la opcion deseada"
+	Leer opc
+	
+	Segun opc Hacer
+		1:
+			Escribir "Hamburguesa"
+		2:
+			Escribir "Pizza"
+		3:
+			Escribir "Pollo Frito"
+		4:
+			Escribir "Ensalada"
+		De Otro Modo:
+			Escribir "Opción Invalida"
+	Fin Segun
+FinAlgoritmo
